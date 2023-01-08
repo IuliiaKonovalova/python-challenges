155368a9e19ce47abdf6be160010887395e49e81
IuliiaKonovalova <juliiakonn@protonmail.com>
2023-01-08 11:24:57 +0700

    add: testcase for card masking function

--- challenge1.py
+++ challenge1.py
@@ -16,14 +16,27 @@ print("Masked String: " + "*" * mask + showstr)
 
 # second solution
 def masking(card_number):
-    return str(card_number[-4:]).rjust(len(str(card_number)), "*")
+    return str(card_number)[-4:].rjust(len(str(card_number)), "*")
 
-print(masking("1234567891234567"))
+print(masking(1234567891234567))
 
 class TestMaskCardTest(unittest.TestCase):
     def test_mask_card(self):
         self.assertEqual(masking("1234567891234567"), "************4567")
 
+
+class TestMaskCardTest2(unittest.TestCase):
+
+    param_list = [
+        ("1234567891234567", "************4567"),
+        (1234567891234567, "************4567"),
+    ]
+
+    def test_return_type(self):
+        for par1, par2 in self.param_list:
+            print(par1, par2)
+            with self.subTest():
+                self.assertEqual(masking(par1), par2)
 
 if __name__ == '__main__':
     unittest.main()
