74006a3a1346cbe70c9bc11d11fb99f8a826fb0b
IuliiaKonovalova <juliiakonn@protonmail.com>
2023-01-08 11:59:33 +0700

    add: testcase for second challenge

--- challenge2.py
+++ challenge2.py
@@ -1,15 +1,46 @@
 import os
 import sys
+import unittest
 
-path = "just_text.txt"
+file_path = "just_text.txt"
+file_path2 = "just_text2.txt"
+
 
 try:
-
-    size = os.path.getsize(path)
+    size = os.path.getsize(file_path)
 
 except FileNotFoundError:
-
     print("File not found")
     sys.exit()
 
 print("File size is", size, "bytes")
+
+# solution2
+
+def get_file_size(file_path_ex):
+    try:
+        size = os.path.getsize(file_path_ex)
+        return print("File size: ", size, "bytes.")
+    except FileNotFoundError:
+        return "File not found"
+
+
+get_file_size(file_path)
+get_file_size(file_path2)
+
+class TestGetFileSize(unittest.TestCase):
+
+    param_list = [
+        # ("just_text.txt", "File size: is 38 bytes."),
+        (file_path2, "File not found"),
+    ]
+
+    def test_return(self):
+        for par1, par2 in self.param_list:
+            print(par1, par2)
+            with self.subTest():
+                self.assertEqual(get_file_size(par1), par2)
+
+
+if __name__ == '__main__':
+    unittest.main()
