0a98fd79f15239beeff0d35161612307c661f73e
IuliiaKonovalova <juliiakonn@protonmail.com>
2023-01-14 16:25:11 +0700

    Add test case for lottery challenge

--- challenge5.py
+++ challenge5.py
@@ -12,7 +12,20 @@ def lottery_winner(num_list, amount):
 
 print(lottery_winner(numbers, amount_of_numbers))
 
+class TestLotteryWinner(unittest.TestCase):
 
+    param_list = [
+        ([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 6,),
+        ([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 5,),
+        ([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 4,),
+        ([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3,),
+    ]
+
+    def test_return_type(self):
+        for par1, par2 in self.param_list:
+            with self.subTest():
+                self.assertIsInstance(lottery_winner(par1, par2), list)
+                self.assertEqual(len(lottery_winner(par1, par2)), par2)
 
 
 if __name__ == '__main__':
