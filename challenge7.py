830334a35b900172c0e608213a13974f23054b74
IuliiaKonovalova <juliiakonn@protonmail.com>
2023-01-30 19:08:45 +0700

    Add testcase for twoSum challenge

--- challenge7.py
+++ challenge7.py
@@ -17,5 +17,13 @@ class Solution(object):
                 checked_value_index[nums[i]] = i
 
 
+class TestSolution(unittest.TestCase):
+    def test_twoSum(self):
+        self.assertEqual(Solution().twoSum([2, 7, 11, 15], 9), [0, 1])
+        self.assertEqual(Solution().twoSum([3, 2, 4], 6), [1, 2])
+        self.assertEqual(Solution().twoSum([3, 3], 6), [0, 1])
+        self.assertEqual(Solution().twoSum([3, 2, 3], 6), [0, 2])
+
+
 if __name__ == '__main__':
   unittest.main()
